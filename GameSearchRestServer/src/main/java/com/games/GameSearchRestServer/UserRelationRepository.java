23025c294284f81e1a8836f0edb897ac77b6ac5b
mati <mati@DESKTOP-90KPIC7>
2019-11-15 14:16:27 +0100

    Viernes

--- GameSearchRestServer/src/main/java/com/games/GameSearchRestServer/UserRelationRepository.java
+++ GameSearchRestServer/src/main/java/com/games/GameSearchRestServer/UserRelationRepository.java
@@ -1,9 +1,17 @@
 package com.games.GameSearchRestServer;
 
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface UserRelationRepository extends CrudRepository<UserRelation,String>{
-
+	
+	
+	  @Query("Select u from UserRelation u where u.userid = ?1") 
+	  public Iterable<UserRelation> findbyUserId(String userid);
+	 
+	
 }
--- GameSearchRestServer/src/main/java/com/games/GameSearchRestServer/UserRelationRestController.java
+++ GameSearchRestServer/src/main/java/com/games/GameSearchRestServer/UserRelationRestController.java
@@ -1,17 +1,26 @@
 package com.games.GameSearchRestServer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/userrelationapi")
+@RequestMapping("/webapi")
 public class UserRelationRestController {
 	
 	@Autowired
 	UserRelationRepository repository;
+	
+	
+	
+	
+	  @GetMapping("/userrelationss") 
+	  public Iterable<UserRelation> findbyUserId(String userid){
+		  return repository.findbyUserId(userid); }
+	 
 	
 	@PostMapping(path="/userrelations",consumes="aplication/json")
 	public void addUserRelation(@RequestBody UserRelation rel) {
